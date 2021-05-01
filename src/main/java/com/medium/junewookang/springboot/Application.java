f163211571456aeeebe94b1cab94bb3261d65952
KangJunewoo <brianjune.dev@gmail.com>
2021-05-01 16:50:42 +0900

    feat: 간단한 스프링 프로젝트 & 테스트코드 작성 및 실행

---
+++ src/main/java/com/medium/junewookang/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.medium.junewookang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // bean 읽기&생성 자동 설정. 프로젝트 최상단에 위치해야 함.
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args); // 내장 WAS 실행.
+    }
+}
---
+++ src/test/java/com/medium/junewookang/springboot/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.medium.junewookang.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)// 스프링부트테스트와 junit 사이에 낑기는 애노테이션
+@WebMvcTest(controllers = HelloController.class) // 난 컨트롤러를 테스트 할거야
+public class HelloControllerTest {
+    @Autowired // 스프링 빈을 주입받고
+    private MockMvc mvc; // api 테스트용 변수 선언
+
+    @Test
+    public void returnsHello() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
