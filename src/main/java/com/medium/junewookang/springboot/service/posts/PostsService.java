fa24e01cca0700d370fd93bdb821c266dd2e1226
KangJunewoo <brianjune.dev@gmail.com>
2021-05-07 10:03:20 +0900

    feat: POST요청 컨트롤러 작성 (다음 진도 나가기 전 이번 커밋 복습 필요!!)

---
+++ src/main/java/com/medium/junewookang/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.medium.junewookang.springboot.service.posts;
+
+import com.medium.junewookang.springboot.domain.posts.PostsRepository;
+import com.medium.junewookang.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
