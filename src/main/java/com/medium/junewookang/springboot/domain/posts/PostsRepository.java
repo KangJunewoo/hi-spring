cdb205c4746cfc23cc777af4a89ce242b70bebac
KangJunewoo <brianjune.dev@gmail.com>
2021-05-07 09:40:44 +0900

    feat: Spring Boot JPA 코드 작성

---
+++ src/main/java/com/medium/junewookang/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.medium.junewookang.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+}
