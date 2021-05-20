3c9787f9df46adc229a81ecb84fd7dc2df820b62
KangJunewoo <brianjune.dev@gmail.com>
2021-05-20 11:55:49 +0900

    reafactor: 어노테이션 직접 만들어 기존 세션 관련 코드 모듈화 & 개선

---
+++ src/main/java/com/medium/junewookang/springboot/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.medium.junewookang.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER) // 메소드 파라미터에 붙이는 용도로 쓰겠다.
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser { // 어노테이션 이름은 LoginUser로 하겠다.
+}
---
+++ src/main/java/com/medium/junewookang/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,30 @@
+package com.medium.junewookang.springboot.config.auth;
+
+import com.medium.junewookang.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter){ // 특정 파라미터를 적용하는지 판단
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception{ // 파라미터에 객체 전달
+        return httpSession.getAttribute("user");
+    }
+}
