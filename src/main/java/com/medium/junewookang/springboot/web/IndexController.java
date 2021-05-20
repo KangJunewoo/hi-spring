7273a9c90ad662c2c5a27ba92373745c00f0b9fc
KangJunewoo <brianjune.dev@gmail.com>
2021-05-20 11:55:18 +0900

    feat: IndexController에 세션 관련 코드 추가

--- src/main/java/com/medium/junewookang/springboot/web/IndexController.java
+++ src/main/java/com/medium/junewookang/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.medium.junewookang.springboot.web;
 
+import com.medium.junewookang.springboot.config.auth.dto.SessionUser;
 import com.medium.junewookang.springboot.service.posts.PostsService;
 import com.medium.junewookang.springboot.web.dto.PostsResponseDto;
 import com.medium.junewookang.springboot.web.dto.PostsUpdateRequestDto;
@@ -11,14 +12,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
