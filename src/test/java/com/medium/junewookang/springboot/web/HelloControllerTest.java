be86887785d733314eadb07763e22ed327be2f13
KangJunewoo <brianjune.dev@gmail.com>
2021-05-20 14:13:09 +0900

    feat: 테스트 정상작동하도록 코드 수정 및 버전업

--- src/test/java/com/medium/junewookang/springboot/web/HelloControllerTest.java
+++ src/test/java/com/medium/junewookang/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.medium.junewookang.springboot.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.medium.junewookang.springboot.config.auth.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -12,14 +16,17 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)// 스프링부트테스트와 junit 사이에 낑기는 애노테이션
-@WebMvcTest(controllers = HelloController.class) // 난 컨트롤러를 테스트 할거야
+@ExtendWith(SpringExtension.class)// 스프링부트테스트와 junit 사이에 낑기는 애노테이션
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)}
+) // 난 컨트롤러를 테스트 할거야
 public class HelloControllerTest {
     @Autowired // 스프링 빈을 주입받고
     private MockMvc mvc; // api 테스트용 변수 선언
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void returnsHello() throws Exception{
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -27,13 +34,14 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void returnsHelloDto() throws Exception{
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(get("/hello/dto")
-            .param("name", name).param("amount", String.valueOf(amount))
+                .param("name", name).param("amount", String.valueOf(amount))
         ).andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
--- src/test/java/com/medium/junewookang/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/medium/junewookang/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,6 @@
 package com.medium.junewookang.springboot.web.dto;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
